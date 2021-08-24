91c4fe5c255a075417bacddffd14cc58c58d9649
linux_china <libing.chen@gmail.com>
2021-08-24 11:29:48 -0700

    Compiled by updated source code

--- lib/common/i18n.d.ts
+++ lib/common/i18n.d.ts
@@ -1,5 +1,6 @@
+import { I18n } from 'i18n';
 export declare function getConfig(key: string): any;
 export declare function getProfileFile(): {};
 export declare function getDefaultProfilePath(): string;
-declare const i18n: any;
+declare const i18n: I18n;
 export default i18n;
